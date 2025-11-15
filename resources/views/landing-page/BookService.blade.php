1cc513b64aef2ba0fc1d8d62dd5e72833d10d11f
Abdul Hadi <manzarnazar@gmail.com>
2025-11-15 10:59:25 +0500

    first commit

---
+++ resources/views/landing-page/BookService.blade.php
@@ -0,0 +1,13 @@
+@extends('landing-page.layouts.default')
+
+@section('content')
+    <div class="section-padding">
+        <div class="container">
+            <booking-wizard :service="{{ $service }}" :shop_list="{{ $shop_list }}" :shop_id="'{{ $shop_id }}'"
+                :coupons="{{ $coupons }}" :taxes="{{ $taxes }}" :user_id="{{ $user_id }}"
+                :availableserviceslot="{{ json_encode($availableserviceslot) }}"
+                :serviceaddon="{{ isset($serviceaddon) ? $serviceaddon : 'null' }}" :googlemapkey="'{{ $googlemapkey }}'"
+                :wallet_amount="{{ $wallet_amount }}"></booking-wizard>
+        </div>
+    </div>
+@endsection
